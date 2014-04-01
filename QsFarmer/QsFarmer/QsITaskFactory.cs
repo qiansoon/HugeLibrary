2857b8b8cf55e7499e843b611014399ce242352e
Zhao Yi Min <ym.zhao@qiansoon.com>
2014-04-01 19:22:43 +0800

    New code.
    Signed-off-by: Zhao Yi Min <ym.zhao@qiansoon.com>

--- QsFarmer/QsFarmer/QsITaskFactory.cs
+++ QsFarmer/QsFarmer/QsITaskFactory.cs
@@ -5,7 +5,22 @@ using System.Text;
 
 namespace QsFarmer
 {
-    class QsITaskFactory
+    public class QsTaskFactory
     {
+        private static HashSet<QsITaskFactory> taskFactorySet = new HashSet<QsITaskFactory>
+        public static string GetTaskFactory(int type)
+        {
+            return taskFactorySet.ElementAt(type);
+        }
+        public static QsITaskFactory CreateFactory(string factory)
+        {
+            Type t = Type.GetType("QsFarmer." + factory.Trim());
+            return Activator.CreateInstance(t) as QsITaskFactory;
+        }
+    }
+
+    public interface QsITaskFactory
+    {
+        public QsITaskProcessor CreateTaskProcessor();
     }
 }
